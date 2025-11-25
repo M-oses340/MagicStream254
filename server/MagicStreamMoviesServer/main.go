a46997a976f61bedeaad6ded7608c9929281488a
Moses Omwa <mosesomwa7@gmail.com>
2025-11-25 12:50:12 +0300

    initializing Go

--- server/MagicStreamMoviesServer/main.go
+++ server/MagicStreamMoviesServer/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Magic Stream Movies Server")
+	router := gin.Default()
+
+	// Example route
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "MagicStream Movies API is running 🚀",
+		})
+	})
+
+	// Start server on port 8080
+	router.Run(":8080")
 }
