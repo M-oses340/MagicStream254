81d59b2fdf444b7208f9913b85a682822be6216d
Moses Omwa <mosesomwa7@gmail.com>
2025-11-26 07:23:53 +0300

    user model

--- server/MagicStreamMoviesServer/models/user_model.go
+++ server/MagicStreamMoviesServer/models/user_model.go
@@ -9,14 +9,14 @@ import (
 type User struct {
 	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	UserID          string             `bson:"user_id" json:"user_id"`
-	FirstName       string             `bson:"first_name" json:"first_name"`
-	LastName        string             `bson:"last_name" json:"last_name"`
-	Email           string             `bson:"email" json:"email"`
-	Password        string             `bson:"password" json:"password"`
-	Role            string             `bson:"role" json:"role"`
+	FirstName       string             `bson:"first_name" json:"first_name" validate:"required min=2,max=100"`
+	LastName        string             `bson:"last_name" json:"last_name" validate:"required min=2,max=100"`
+	Email           string             `bson:"email" json:"email" validate:"required,email"`
+	Password        string             `bson:"password" json:"password" validate:"required,min=8,max=20"`
+	Role            string             `bson:"role" json:"role" validate:"oneof = ADMIN USER"`
 	CreatedAt       time.Time          `bson:"created_at" json:"created_at"`
 	UpdatedAt       time.Time          `bson:"updated_at" json:"updated_at"`
 	Token           string             `bson:"token" json:"token"`
 	RefreshToken    string             `bson:"refresh_token" json:"refresh_token"`
-	FavouriteGenres []Genre            `bson:"favourite_genres" json:"favourite_genres"`
+	FavouriteGenres []Genre            `bson:"favourite_genres" json:"favourite_genres" validate:"required,dive"`
 }
