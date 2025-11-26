3fa93ebd361ce04d009eb821f6ba9a21e03f4282
Moses Omwa <mosesomwa7@gmail.com>
2025-11-26 20:10:30 +0300

    auth midlleware

--- server/MagicStreamMoviesServer/utils/token_util.go
+++ server/MagicStreamMoviesServer/utils/token_util.go
@@ -2,10 +2,12 @@ package utils
 
 import (
 	"context"
+	"errors"
 	"os"
 	"time"
 
 	"github.com/M-oses340/MagicStream254/server/MagicStreamMoviesServer/database"
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -90,4 +92,37 @@ func UpdateAllTokens(userId, token, refreshToken string) error {
 	}
 
 	return nil
+}
+func GetAccessToken(c *gin.Context) (string, error) {
+	authHeader := c.Request.Header.Get("Authorization")
+	if authHeader == "" {
+		return "", errors.New("Authorization header is empty")
+
+	}
+	tokenString := authHeader[len("Bearer "):]
+	if tokenString == "" {
+		return "", errors.New("Bearer token is required")
+	}
+	return tokenString, nil
+}
+func ValidateToken(tokenString string) (*SignedDetails, error) {
+	claims := &SignedDetails{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SECRET_KEY), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, err
+	}
+
+	if claims.ExpiresAt.Time.Before(time.Now()) {
+		return nil, errors.New("token has expired")
+	}
+
+	return claims, nil
+
 }
