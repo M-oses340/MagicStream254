4a034096a223298c851ed09aba659d7c113539ba
Moses Omwa <mosesomwa7@gmail.com>
2025-11-29 16:17:02 +0300

    frontend

---
+++ client/magic-stream-client/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import { useState,useEffect } from "react";
+import axiosClient from '../../api/axios.Config';
+import Movies from "../movies/movies";
+
+const Home = () =>{
+    const [movies,setMovies] = useState([])
+}
+
+
+
