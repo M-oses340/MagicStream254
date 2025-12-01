aeb73f732e78612946748aa7ec2f2d64b28f9c23
Moses Omwa <mosesomwa7@gmail.com>
2025-12-01 20:11:19 +0300

    login

--- client/magic-stream-client/src/context/AuthProvider.jsx
+++ client/magic-stream-client/src/context/AuthProvider.jsx
@@ -5,7 +5,7 @@ const AuthContext = createContext({});
 export const AuthProvider = ({children}) => {
     const [auth, setAuth] = useState();
     return(
-         <AuthContext.Provider value = {{auth,setAuth,loading}}>
+         <AuthContext.Provider value = {{auth,setAuth}}>
             {children}
         </AuthContext.Provider>
     );
