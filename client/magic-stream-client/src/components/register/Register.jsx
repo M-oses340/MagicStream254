323ac575bceec6af7a299ac785e5ca1c53da9b70
Moses Omwa <mosesomwa7@gmail.com>
2025-12-01 10:53:33 +0300

    register

--- client/magic-stream-client/src/components/register/Register.jsx
+++ client/magic-stream-client/src/components/register/Register.jsx
@@ -1,8 +1,24 @@
-
+import { useState, useEffect} from 'react';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import axiosClient from '../../api/axiosConfig';
+import { useNavigate, Link } from 'react-router-dom';
 
 const Register = () => {
+     const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [favouriteGenres, setFavouriteGenres] = useState([]);
+    const [genres, setGenres] = useState([]);
+
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
     return(
-        <h2>Register</h2>
+       <Container className="login-container d-flex align-items-center justify-content-center min-vh-100"></Container>
     )
 }
 export default Register;
