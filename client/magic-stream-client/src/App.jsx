055e61062d328be17563af3fe2a762a58140de5a
Moses Omwa <mosesomwa7@gmail.com>
2025-12-01 21:11:39 +0300

    login

--- client/magic-stream-client/src/App.jsx
+++ client/magic-stream-client/src/App.jsx
@@ -5,16 +5,18 @@ import Register from "./components/register/Register";
 import Login from "./components/login/Login";
 import './App.css'
 import {Route, Routes, useNavigate} from 'react-router-dom'
+import Layout from "./components/Layout";
+import RequiredAuth from "./components/RequiredAuth";
 
 function App() {
   return (
     <>
       <Header />
-      <Routes >
+      <Routes path="/" element={<Layout/>}>
         <Route path="/" element={<Home />}></Route>
         <Route path="/register" element={<Register/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
-        
+        <Routes path="/" element={<RequiredAuth/>}></Routes>
       </Routes>
     </>
   );
