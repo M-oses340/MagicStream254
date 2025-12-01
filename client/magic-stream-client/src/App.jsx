6210ef36705495b23843e40705c70a1ad35c4cb1
Moses Omwa <mosesomwa7@gmail.com>
2025-12-01 10:15:35 +0300

    frontend

--- client/magic-stream-client/src/App.jsx
+++ client/magic-stream-client/src/App.jsx
@@ -1,13 +1,25 @@
 import Home from "./components/home/Home";
-import Header from "./components/header/Header"
-import { useState, useEffect } from 'react'
+import Header from "./components/header/Header";
+import { useState, useEffect } from 'react';
+import Register from "./components/register/Register";
+import Login from "./components/login/Login";
 import './App.css'
+import {Route, Routes, useNavigate} from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Header/>
-      <Home />
+      <Header handleLogout = {handleLogout}/>
+      <Routes path="/" element = {<Layout/>}>
+        <Route path="/" element={<Home updateMovieReview={updateMovieReview}/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route element = {<RequiredAuth/>}>
+            <Route path="/recommended" element={<Recommended/>}></Route>
+            <Route path="/review/:imdb_id" element={<Review/>}></Route>
+            <Route path="/stream/:yt_id" element={<StreamMovie/>}></Route>
+        </Route>
+      </Routes>
     </>
   );
 }
