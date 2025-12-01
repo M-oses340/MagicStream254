7ad59aa9098cdcf7abecaf7163915581937b6f4f
Moses Omwa <mosesomwa7@gmail.com>
2025-12-01 10:21:28 +0300

    login/register

--- client/magic-stream-client/src/App.jsx
+++ client/magic-stream-client/src/App.jsx
@@ -9,16 +9,12 @@ import {Route, Routes, useNavigate} from 'react-router-dom'
 function App() {
   return (
     <>
-      <Header handleLogout = {handleLogout}/>
-      <Routes path="/" element = {<Layout/>}>
-        <Route path="/" element={<Home updateMovieReview={updateMovieReview}/>}></Route>
+      <Header />
+      <Routes >
+        <Route path="/" element={<Home />}></Route>
         <Route path="/register" element={<Register/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
-        <Route element = {<RequiredAuth/>}>
-            <Route path="/recommended" element={<Recommended/>}></Route>
-            <Route path="/review/:imdb_id" element={<Review/>}></Route>
-            <Route path="/stream/:yt_id" element={<StreamMovie/>}></Route>
-        </Route>
+        
       </Routes>
     </>
   );
