bc6714f3cb49f2cb34831d184fe29f1184bcf6e0
Moses Omwa <mosesomwa7@gmail.com>
2025-12-03 07:24:38 +0300

    home

--- client/magic-stream-client/src/App.jsx
+++ client/magic-stream-client/src/App.jsx
@@ -1,32 +1,60 @@
-import Home from "./components/home/Home";
-import Header from "./components/header/Header";
-import { useState, useEffect } from 'react';
-import Register from "./components/register/Register";
-import Login from "./components/login/Login";
-import Recommended from './components/recommended/Recommended';
+import { useState, useEffect } from 'react'
 import './App.css'
-import {Route, Routes, useNavigate} from 'react-router-dom'
-import Layout from "./components/Layout";
-import RequiredAuth from "./components/RequiredAuth";
+import Home from './components/home/Home';
+import Recommended from './components/recommended/Recommended';
 import Review from './components/review/Review';
-import StreamMovie from "./components/stream/StreamMovie";
+import Header from './components/header/Header';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
+import Layout from './components/Layout';
+import RequiredAuth from './components/RequiredAuth';
+import axiosClient from './api/axios.Config';
+import useAuth from './hooks/useAuth';
+import StreamMovie from './components/stream/StreamMovie';
+
+import {Route, Routes, useNavigate} from 'react-router-dom'
 
 function App() {
+
+  const navigate = useNavigate();
+  const { auth, setAuth } = useAuth();
+
+  
+  const updateMovieReview = (imdb_id) => {
+      navigate(`/review/${imdb_id}`);
+  };
+   
+  const handleLogout = async () => {
+
+        try {
+            const response = await axiosClient.post("/logout",{user_id: auth.user_id});
+            console.log(response.data);
+            setAuth(null);
+           // localStorage.removeItem('user');
+            console.log('User logged out');
+
+        } catch (error) {
+            console.error('Error logging out:', error);
+        } 
+
+    };
+
   return (
     <>
-      <Header />
-      <Routes path="/" element={<Layout/>}>
-        <Route path="/" element={<Home />}></Route>
+      <Header handleLogout = {handleLogout}/>
+      <Routes path="/" element = {<Layout/>}>
+        <Route path="/" element={<Home updateMovieReview={updateMovieReview}/>}></Route>
         <Route path="/register" element={<Register/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
-        <Route path="/" element={<RequiredAuth/>}>
-          <Route path="/recommended" element={<Recommended/>}></Route>
-          <Route path="/review/:imdb_id" element={<Review/>}></Route>
-          <Route path="/stream/:yt_id" element={<StreamMovie/>}></Route>
-        </Route>  
+        <Route element = {<RequiredAuth/>}>
+            <Route path="/recommended" element={<Recommended/>}></Route>
+            <Route path="/review/:imdb_id" element={<Review/>}></Route>
+            <Route path="/stream/:yt_id" element={<StreamMovie/>}></Route>
+        </Route>
       </Routes>
+
     </>
-  );
+  )
 }
 
 export default App;
